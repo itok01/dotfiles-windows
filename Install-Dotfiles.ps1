0a0353137878f2bd798726880ac7e4085aaa01b5
unknown <contact@itok01.com>
2020-01-15 01:35:28 +0900

    cahnge copying dotfiles to linking dotfiles

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -5,9 +5,10 @@ Function Install-Dotfiles {
     $dotfiles = Get-Content $dotfilesJsonPath | ConvertFrom-Json
 
     foreach ($dotfile in $dotfiles) {
-        $item = $ExecutionContext.InvokeCommand.ExpandString($dotfile.item)
-        $path = $ExecutionContext.InvokeCommand.ExpandString($dotfile.path)
-        Write-Output "Copying $item to $path ..."
-        Copy-Item $item $path
+        $item = Convert-Path $ExecutionContext.InvokeCommand.ExpandString($dotfile.item)
+        $path = Convert-Path $ExecutionContext.InvokeCommand.ExpandString($dotfile.path)
+        New-Item -Type SymbolicLink -Path $path -Value $item -Force
     }
 }
+
+Install-Dotfiles .\dotfiles.json
